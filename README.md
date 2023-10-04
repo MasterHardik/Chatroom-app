45c20cf0577b7ec08d322c8d979dfcec81845ef9
Hardik Chouhan <96509240+MasterHardik@users.noreply.github.com>
2023-10-04 14:07:19 +0530

    Bug Fixed

--- README.md
+++ README.md
@@ -1,15 +1,57 @@
-# Chatroom-app
+# Chatroom App
 
-Free , Fast , Facile conversation
+## Overview
 
-step 1 : Make sure to install npm dependencies using the following command
+Chatroom App is a free, fast, and easy-to-use platform for real-time conversations. It allows users to join chat rooms and engage in discussions with others in real-time. This simple and intuitive chat application is perfect for staying connected with friends, colleagues, or anyone around the world.
 
-> > npm install
+## Getting Started
 
-step 2 : Run the following command to run the server
+Follow these steps to get started with Chatroom App on your local machine:
 
-> > node server.js
+### Prerequisites
 
-step 3 : open the app in browser using the following address
+Make sure you have Node.js and npm (Node Package Manager) installed on your computer.
 
-    http://localhost:5555/
+### Installation
+
+1. Clone this GitHub repository to your local machine:
+
+   ```bash
+   git clone https://github.com/MasterHardik/chatroom-app.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd chatroom-app
+   ```
+
+3. Install the necessary dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Running the Server
+
+   ```bash
+   node server.js
+   ```
+
+5. Accessing the App : Open your web browser and visit the following address
+
+   http://localhost:5555/
+
+## Usage
+
+On the app's homepage, enter your desired username and click "Join."
+Start chatting with other users in the chatroom.
+Enjoy free and fast conversations with friends and colleagues.
+
+## Technologies Used
+
+Frontend: HTML, CSS, JavaScript
+
+Backend: Node.js, Express.js, Socket.IO
+
+Enjoy fast and convenient conversations with Chatroom App!
--- public/code.js
+++ public/code.js
@@ -1,13 +1,12 @@
-// 4. Creating socket and listners
 (function () {
     const app = document.querySelector(".app");
     const socket = io();
 
     let uname;
+
     app.querySelector(".join-screen #join-user").addEventListener("click", function () {
-        alert("fdsfds");
         let username = app.querySelector(".join-screen #username").value;
-        if (username.length == 0) {
+        if (username.length === 0) {
             return;
         }
         socket.emit("newuser", username);
@@ -15,53 +14,64 @@
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
     });
-    
-    app.querySelector(".chat-screen #send-message").addEventListener("click", function (){
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
         let message = app.querySelector(".chat-screen #message-input").value;
-    if (message.length == 0) {
-        return;
-    }
-    renderMessage("my", {
-        username: uname,
-        text: message
-    });
+        if (message.length === 0) {
+            return;
+        }
+        renderMessage("my", {
+            username: uname,
+            text: message
+        });
         app.querySelector(".chat-screen #message-input").value = "";
+        socket.emit("chat", { username: uname, text: message });
     });
+
     app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
         socket.emit("exituser", uname);
-        window.locationhref = window.location.href;
+        window.location.reload(); // Reload the page to exit the chat
     });
 
     socket.on("update", function (update) {
         renderMessage("update", update);
     });
+
     socket.on("chat", function (message) {
-        renderMessage("other", update);
+        renderMessage("other", message);
     });
-    
+
     function renderMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
-        if (type == "my") {
-            
-        }
-        else if (type == "other") {
+
+        if (type === "my") {
             let el = document.createElement("div");
             el.setAttribute("class", "message my-message");
             el.innerHTML = `
                 <div>
-                    <div> class "name">You</div
+                    <div class="name">${message.username}</div>
                     <div class="text">${message.text}</div>
                 </div>
-            `
-        }
-        else if (type == "update") {
+            `;
+            messageContainer.appendChild(el);
+        } else if (type === "other") {
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message");
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        } else if (type === "update") {
             let el = document.createElement("div");
             el.setAttribute("class", "update");
             el.innerHTML = message;
             messageContainer.appendChild(el);
-            
         }
-        // scroll chat to end
-        messageContainer.scrollTo = messageContainer.scrollHeight - messageContainer.clientHeight;
+
+        // Scroll chat to the end
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 })();
